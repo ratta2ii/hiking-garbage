15e6cddd1faf1863db8552861e920dc9da6149c5
Jordan Safford and Trevor Mackin <trevor@>
2019-12-03 17:04:19 -0800

    Add Dark Sky weather API. Add Google Maps distance API. Add Hiking Project API. Use the longitude and latitude fromthe Hiking Project API to call the  weather API in order to receive information about the weather for a specific location.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { findTrails } from './scripts';
+import { findTrails } from './apis/trails';
+import { findWeather } from './apis/weather';
+//import { findCampgrounds } from './apis/camping';
+//import { findDistance } from './apis/distance';
 
 
 $(document).ready(function() {
@@ -16,19 +19,31 @@ $(document).ready(function() {
         $("#display-results").empty();
         const body = JSON.parse(response);
         const trails = body.trails;
-        console.log(trails);
-
-        
-
+   
         for (let i in trails){
-            if (trails[i].length > maxHikeDistance){
-              $("#display-results").append(`<li>${trails[i].name}</li><li>${trails[i].location}</li><li>${trails[i].length} mile hike</li><li>${trails[i].latitude}</li><li>${trails[i].longitude}</li><p></p>`);
-            }
-        }
-        
-        
-      }
-      , function(error) {
+          if (trails[i].length < maxHikeDistance) {
+
+            let trailCoordinates = `${trails[i].latitude},${trails[i].longitude}`;
+
+/* Dark Sky Weather API. Trail coordinates are used as arguments to find weather for that area. */  
+
+            findWeather(trailCoordinates)
+              .then((response) => {
+
+                let weatherBody = JSON.parse(response);
+                let weatherSummary = weatherBody.daily.data[0].summary; 
+                let temperature = weatherBody.daily.data[0].temperatureHigh; 
+
+                $("#display-results").append(`<ul><li>${trails[i].name}</li><li>${trails[i].location}</li><li>${trails[i].length} mile hike</li><li>${trails[i].latitude}</li><li>${trails[i].longitude}</li><li>${weatherSummary}</li><li>${temperature} degree high</li></ul>`);
+              },
+              function(error) {
+                console.error(`I am the error message: ${error.message}`);
+              });
+          }
+
+        }      
+      },
+      function(error) {
         $("#display-results").empty().append(`<h5>There was an error processing your request: ${error.message}</h5>`);
         $("#display-div").show();
       });
@@ -40,7 +55,37 @@ $(document).ready(function() {
 
 
 
-// 'https://www.google.com/maps/dir/?api=1&destination=' . $address
 
 
+
+
+
+
+
+
+// findCampgrounds(trails[i].latitude, trails[i].longitude)
+// .then((response) => {
+//   const body = JSON.parse(response);
+//   let campgrounds = body.campgrounds;
+// },
+// function(error) {
+//   console.error(`I am the error message: ${error.message}`);
+// });
+
+
+
+
+
+// let trailCoordinates = `${trails[i].latitude},${trails[i].longitude}`;
+
+// findDistance(trailCoordinates);
+// .then((response) => {
+
+//   let distanceBody = JSON.parse(response);
+//   let distance = distanceBody.rows[0].elements[0].distance.text; 
+//   let duration = distanceBody.rows[0].elements[0].duration.text;  
+// },
+// function(error) {
+//   console.error(`I am the error message: ${error.message}`);
+// });
 
